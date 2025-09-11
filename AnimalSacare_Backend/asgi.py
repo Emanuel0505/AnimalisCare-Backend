ccad8ec0d832e9de1c78112fe8416e876a307658
Emanuel João <vivi.emanuel05@gmail.com>
2025-09-11 01:12:00 -0300

    Instalação Django
    Foi realizado a instalação do Django no repositório e sua configuração.  Também foi criafo o primeiro app chamado "blog".

---
+++ AnimalSacare_Backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for AnimalSacare_Backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AnimalSacare_Backend.settings')
+
+application = get_asgi_application()
