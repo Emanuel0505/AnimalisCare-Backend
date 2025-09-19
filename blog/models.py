c2c28dce311e4d4ef16642341334eae6a7fe6096
Emanuel João <vivi.emanuel05@gmail.com>
2025-09-19 14:21:06 -0300

    Tags
    na pagina views na função blog_home, fiz a filtragem das tags.

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,11 @@ class BlogPost(models.Model):
     autor = models.CharField(max_length=100, verbose_name="Autor")
     data_criada = models.DateField(auto_now_add=True, verbose_name="Data de Criação")
     imagem = models.ImageField(upload_to='blog_images/%Y/%m/%d/')
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
 
 
+class Tag(models.Model):
+    nome = models.CharField(max_length=50, verbose_name="Nome da Tag")
+    descricao = models.TextField(verbose_name="Descrição da Tag")
+    cor = models.CharField(max_length=7, verbose_name="Cor da Tag (Hex)", default="#FFFFFF")
+    
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, redirect
-from .models import BlogPost
+from django.db.models import Count, Q
+from .models import BlogPost, Tag
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def blog_home(request):
-    blog_post = BlogPost.objects.all()
+    blog_post = BlogPost.objects.all().prefetch_related('tags')
 
+    # Filtragem por múltiplas tags (todas devem estar presentes)
+    selected_tags = request.GET.getlist('tags')
+    if selected_tags:
+        blog_post = (
+            BlogPost.objects.filter(tags__nome__in=selected_tags)
+            .annotate(num_tags=Count("tags", filter=Q(tags__nome__in=selected_tags), distinct=True))
+            .filter(num_tags=len(selected_tags))
+        )
+
+
+    # Paginação
     pagina = request.GET.get('pagina', 1)
     paginator = Paginator(blog_post, 6)
 
@@ -18,6 +30,8 @@ def blog_home(request):
 
     context = {
         'blog_post': result,
+        'all_tags': Tag.objects.all(),
+        'selected_tags': selected_tags,
     }
 
     return render(request, 'index.html', context)
@@ -38,5 +52,19 @@ def create_post(request):
         )
 
         blog_post.save()
+
+    return render(request, 'blog_create.html')
+
+def create_tag(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        
+        tag = Tag(
+            nome=nome,
+            descricao=descricao
+        )
+
+        tag.save()
 
     return render(request, 'blog_create.html')
