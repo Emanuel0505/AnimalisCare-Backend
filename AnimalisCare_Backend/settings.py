37f234dbb84bd023e8c23969b4a7b88fa7f2fead
Emanuel João <vivi.emanuel05@gmail.com>
2025-09-23 00:55:41 -0300

    configurando o .gitignore
    configuralçao, aqui é mais um teste para ver se deu certo.

--- AnimalisCare_Backend/settings.py
+++ AnimalisCare_Backend/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'core/static/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
