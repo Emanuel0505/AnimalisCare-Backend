cb2eaf648723025050dd3760f92441c325b54c9a
Emanuel João <vivi.emanuel05@gmail.com>
2025-09-23 00:37:29 -0300

    Melhoria dos autores no blog
    Agora pode escolher quem é o autor com base na equipe da veterinharia.

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from account.models import User
 
 # Create your models here.
 class BlogPost(models.Model):
     titulo = models.CharField(max_length=200, verbose_name="Título")
     conteudo = models.TextField(verbose_name="Conteúdo")
-    autor = models.CharField(max_length=100, verbose_name="Autor")
+    autor = models.ManyToManyField(User, related_name='posts', verbose_name="Autor(es)")
     data_criada = models.DateField(auto_now_add=True, verbose_name="Data de Criação")
     imagem = models.ImageField(upload_to='blog_images/%Y/%m/%d/')
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
