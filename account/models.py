5f5d7553cef523ad3a77ed469ffb1531d42b9e04
Emanuel João <vivi.emanuel05@gmail.com>
2025-09-23 21:47:41 -0300

    erro quando fez pull request

--- account/models.py
+++ account/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nomeCompleto = models.CharField(max_length=255, verbose_name="Nome Completo")
     biografia = models.TextField(blank=True, null=True, verbose_name="Biografia")
     cargo = models.CharField(max_length=100, blank=True, null=True, verbose_name="Cargo")
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class BlogPost(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     titulo = models.CharField(max_length=200, verbose_name="Título")
     conteudo = models.TextField(verbose_name="Conteúdo")
     autor = models.CharField(max_length=100, verbose_name="Autor")
@@ -12,6 +14,7 @@ class BlogPost(models.Model):
 
 
 class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=50, verbose_name="Nome da Tag")
     descricao = models.TextField(verbose_name="Descrição da Tag")
     cor = models.CharField(max_length=7, verbose_name="Cor da Tag (Hex)", default="#FFFFFF")
